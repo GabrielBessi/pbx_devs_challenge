999b93afe85fa3b4d5565aff180234f65ff799d9
Gabriel Bessi <tecbessi@gmail.com>
2023-02-18 19:26:59 -0300

    feat: Configurando sequelizer

--- src/database/database.mysql.ts
+++ src/database/database.mysql.ts
@@ -1,22 +1,8 @@
-import "dotenv";
-import mysql from "mysql2";
 import { Sequelize } from "sequelize";
 import config from "./config.database";
 import "dotenv";
 
-const connectMysql = async () => {
-  const { host, port, user, password, database } = config;
-  const connection = await mysql.createConnection({
-    host,
-    port,
-    user,
-    password,
-  });
-  await connection.query(`CREATE DATABASE IF NOT EXISTS ${database};`);
-
-  const sequelize = new Sequelize(database, user, password, {
-    dialect: process.env.MYSQL_DIALECT,
-  });
-};
-
-export default connectMysql;
+const sequelize = new Sequelize("database", "username", "password", {
+  host: "localhost",
+  dialect: "mysql",
+});
